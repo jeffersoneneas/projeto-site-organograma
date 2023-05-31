dc98495059cf688184dc7ac494ff4a9d5b62329a
Jefferson Eneas da Silva <jeffersoneneassilva@gmail.com>
2023-05-31 12:40:20 -0300

    validação-lista-suspensa-e-botao

--- src/componentes/Formulario/index.js
+++ src/componentes/Formulario/index.js
@@ -1,14 +1,40 @@
+import Botao from '../Botao'
 import CampoTexto from '../CampoTexto'
+import ListaSuspensa from '../ListaSuspensa'
 import './Formulario.css'
 
 const Formulario = () => {
+
+    const generos = [
+        "Ação",
+        "Aventura",
+        "Animação",
+        "Comédia",
+        "Drama",
+        "Ficção ",
+        "Terror",
+        "Romance",
+        "Suspense",
+        "Documentário",
+        "fantasia",
+        "musical",
+        "guerra",
+        "histórico"
+    ]
+
+    const aoSalvar = (evento) => {
+        evento.preventDefault()
+    }
     return (
         <section className='formulario'>
-            <form >
+            <form onSubmit={aoSalvar}>
                 <h2>Preencha os dados para criar o card do Filme</h2>
-                <CampoTexto label="Nome" placeholder="Digite o nome" />
-                <CampoTexto label="Gênero" placeholder="Digite o gênero do filme" />
+                <CampoTexto obrigatorio={true} label="Nome" placeholder="Digite o nome" />
                 <CampoTexto label="Imagem" placeholder="Digite o endereço da imagem" />
+                <ListaSuspensa obrigatorio={true} label="Gênero" itens={generos} />
+                <Botao>
+                    Criar Card
+                </Botao>
             </form>
         </section>
     )
